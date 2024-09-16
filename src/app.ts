bac25a95699cfcb14a4541b7fbd2b32a4e661496
abhishek701428 <abhishek@hitech.ai>
2024-09-16 13:59:59 +0530

    all

--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,15 @@ connectToMongo();
 
 
 
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    'allowedHeaders': ['Authorization', 'Content-Type']
-}))
+    origin: '*'
+  }));
+
 
   
 app.use('/api', router);
